38803edd17cae2bc2090fc85a2739695ddcec766
yanquanfahei <1449826851@qq.com>
2025-08-25 23:42:34 +0800

    feat: 实现 jsx

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,55 @@
+import type { ElementType, Key, Props, ReactElementType, Ref, Type } from 'shared/ReactTypes'
+// ReactElement
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols'
+
+function ReactElement(type: Type, key: Key, ref: Ref, props: Props): ReactElementType {
+  const element = {
+    $$typeof: REACT_ELEMENT_TYPE,
+    type,
+    key,
+    ref,
+    props,
+    __mark: 'ReactElement',
+  }
+  return element
+}
+
+export function jsx(type: ElementType, config: any, ...maybeChildren: any) {
+  let key: Key = null
+  const props: Props = {}
+  let ref: Ref = null
+
+  for (const prop in config) {
+    const val = config[prop]
+    if (prop === 'key') {
+      if (val !== undefined) {
+        key = `${val}`
+      }
+      continue
+    }
+    if (prop === 'ref') {
+      if (val !== undefined) {
+        ref = `${val}`
+      }
+      continue
+    }
+    if (Object.prototype.hasOwnProperty.call(config, prop)) {
+      props[prop] = val
+    }
+  }
+
+  const maybeChildrenLength = maybeChildren.length
+
+  if (maybeChildrenLength) {
+    if (maybeChildrenLength === 1) {
+      props.children = maybeChildren[0]
+    }
+    else {
+      props.children = maybeChildren
+    }
+  }
+
+  return ReactElement(type, key, ref, props)
+}
+
+export const jsxDEV = jsx
